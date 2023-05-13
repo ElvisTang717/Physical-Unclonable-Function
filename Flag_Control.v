793cfaa6726aaeb726191a32fa6d0f4a7c2e5760
ElvisTang <elvistang717@gmail.com>
2023-05-13 17:47:33 -0600

    Update Flag_Control.v

--- Flag_Control.v
+++ Flag_Control.v
@@ -15,14 +15,7 @@ module Flag_Control(
     input clr_done,
     output reg clr
     );
-//    parameter n = 1;
-//    reg clk_cycle = 0;
-//    always@(posedge clk)begin
-//    if(clk_cycle < n)
-//        clk_cycle = clk_cycle + 1;
-//    else
-//        clk_cycle = 0;
-//    end
+
     reg init = 0;
     reg [7:0] old_chal = 0;
     reg old_btn = 0;
@@ -43,25 +36,11 @@ module Flag_Control(
             clr = 1'b1;
             old_chal = chal;
         end
-//        else
-//            chal_clr = 1'b0;
+
     end
     if(old_btn != btn)
         clr = 1'b1;
-//    else begin
-//        btn_clr = 1'b0;
-//    end
-    
-//    assign clr = chal_clr | btn_clr;
-//        if(clr_done == 1'b1
-//            clr = 1'b0;
-//        else begin
-//            clr = 1'b1;
-//        end
+
     end
     
-//    always @(chal, btn)
-//    begin
-//    clr = 1'b1;
-//    end
 endmodule
